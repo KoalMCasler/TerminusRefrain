2fab83210f1801bc1089a22ace5151393add1d2c
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-10-25 09:21:55 -0300

    Added basic heavy attack with placeholder animation

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -10,8 +10,10 @@ public class PlayerCombat : MonoBehaviour
     public LayerMask EnemyLayers;
 
     public float AttackRange = 0.05f;
-    public int BaseDamage = 1;
+    public int LightDamage = 1;
     public float WaitTime = 0.5f;
+    public int HeavyDamage = 2;
+    public bool WeaponIsHeavy;
   
     // Input check
     private void Update()
@@ -19,26 +21,33 @@ public class PlayerCombat : MonoBehaviour
         // Input
         if (Input.GetButtonDown("MeleeAttack"))
         {
-            StartCoroutine(MeleeAttack());
+            if(WeaponIsHeavy != true)
+            {
+                StartCoroutine(LightMeleeAttack());
+            }
+            if(WeaponIsHeavy == true)
+            {
+                StartCoroutine(HeavyMeleeAttack());
+            }
         }
 
     }
 
     // Melee attack logic
-    private IEnumerator MeleeAttack()
+    private IEnumerator LightMeleeAttack()
     {
         //Starts Attack Animation
-        animator.SetBool("Attacking", true);
+        animator.SetBool("AttackLight", true);
         // Detect enemies in range
         Collider2D[] HitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, EnemyLayers);
         // apply damage
         foreach(Collider2D HitEnemy in HitEnemies)
         {
-            Debug.Log("You hit " + HitEnemy.name);
-            HitEnemy.GetComponent<Enemy>().TakeDamage(BaseDamage);
+            Debug.Log("You hit " + HitEnemy.name + " for " + LightDamage + " damage");
+            HitEnemy.GetComponent<Enemy>().TakeDamage(LightDamage);
         }
         yield return new WaitForSeconds(WaitTime);
-        animator.SetBool("Attacking", false);
+        animator.SetBool("AttackLight", false);
     }
 
     // Debug tool to see the damage zone when in the editor. 
@@ -48,6 +57,22 @@ public class PlayerCombat : MonoBehaviour
             {return;}
         
         Gizmos.DrawWireSphere(AttackPoint.position, AttackRange);
+    }
+    private IEnumerator HeavyMeleeAttack()
+    {
+
+        //Starts Attack Animation
+        animator.SetBool("AttackHeavy", true);
+        // Detect enemies in range
+        Collider2D[] HitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, EnemyLayers);
+        // apply damage
+        foreach(Collider2D HitEnemy in HitEnemies)
+        {
+            Debug.Log("You hit " + HitEnemy.name + " for " + HeavyDamage + " damage");
+            HitEnemy.GetComponent<Enemy>().TakeDamage(HeavyDamage);
+        }
+        yield return new WaitForSeconds(WaitTime);
+        animator.SetBool("AttackHeavy", false);
     }
 }
 
