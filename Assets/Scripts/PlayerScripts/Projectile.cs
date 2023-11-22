a143b20e11da8e6e380f13fa064b45f02db3995f
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-22 09:20:25 -0400

    Moved targeting to combat script, fixed projectile living forever

--- Assets/Scripts/PlayerScripts/Projectile.cs
+++ Assets/Scripts/PlayerScripts/Projectile.cs
@@ -4,47 +4,37 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private GameObject target;
+    public GameObject target;
     private GameObject selfObject;
     private Rigidbody2D self;
-    public GameObject[] Enemies;
-    public int shotRange;
     private GameObject player;
     public float shotSpeed;
+    public float shotLifeSpan;
     // Start is called before the first frame update
     void Start()
     {
         selfObject = GetComponent<GameObject>();
         player = GameObject.FindWithTag("Player");
-        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
         self = GetComponent<Rigidbody2D>();
-        GetTarget();
         if(target != null)
         {
             Vector3 direction = target.transform.position - transform.position;
             self.velocity = new Vector2(direction.x, direction.y).normalized * shotSpeed;
+            target = null;
         }
         else
         {
             Debug.Log("No Valid Targets");
-            Destroy(selfObject);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    void GetTarget()
-    {
-       foreach (var obj in Enemies)
+        shotLifeSpan -= Time.deltaTime;
+        if(shotLifeSpan <= 0)
         {
-            if (Vector3.Distance(player.transform.position, obj.transform.position) < shotRange)
-            {
-                target = obj;
-                break;
-            }
-        } 
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerScripts/RangedCombat.cs
+++ Assets/Scripts/PlayerScripts/RangedCombat.cs
@@ -1,37 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RangedCombat : MonoBehaviour
 {
+    public GameObject player;
     public GameObject projectile;
+    public GameObject target;
+    public GameObject[] Enemies;
     public Transform projectilePos;
     private int lightTime;
     private int heavyTime;
     public bool WeaponIsHeavy;
+    public int attackRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player"); 
     }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("RangedAttack"))
         {
-            if(WeaponIsHeavy != true)
+            Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            GetTarget();
+            if(target != null && Vector3.Distance(player.transform.position, target.transform.position) < attackRange)
             {
-                StartCoroutine(LightRangedAttack());
+                PassTarget();
+                if(WeaponIsHeavy != true)
+                {
+                    StartCoroutine(LightRangedAttack());
+                }
+                if(WeaponIsHeavy == true)
+                {
+                    StartCoroutine(HeavyRangedAttack());
+                }
             }
-            if(WeaponIsHeavy == true)
+            else
             {
-                StartCoroutine(HeavyRangedAttack());
+                Debug.Log("No Valid Targets");
             }
+        }
+        else
+        {
+            target = null;
         }
     }
     void Shoot()
     {
+
         Instantiate(projectile, projectilePos.position, Quaternion.identity);
     }
     private IEnumerator LightRangedAttack()
@@ -43,5 +64,27 @@ public class RangedCombat : MonoBehaviour
     {
         yield return new WaitForSeconds(heavyTime);
         Shoot();
+    }
+    void GetTarget()
+    {
+       foreach (var obj in Enemies)
+        {
+            if (Vector3.Distance(player.transform.position, obj.transform.position) < attackRange)
+            {
+                target = obj;
+                break;
+            }
+        } 
+    }
+    void PassTarget()
+    {
+        if(target != null)
+        {
+            projectile.GetComponent<Projectile>().target = target;
+        }
+        else
+        {
+            Debug.Log("No Valid Targets");
+        }
     }
 }
